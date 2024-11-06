f2c11f4e82918e1added0f96a2a9a51baacb197b
Rodri Sanchez <rodrigo@Macboock-129.local>
2024-11-05 22:16:29 -0600

    Se agregan comentarios para tener clara la guia de creacion del menu principal del programa
    Signed-off-by: Rodri Sanchez <rodrigo@Macboock-129.local>

--- src/main.cpp
+++ src/main.cpp
@@ -27,3 +27,29 @@ int main(){
     cout << "|                                                                           |" <<  endl;
     cout << "|---------------------------------------------------------------------------|" <<  endl;
 }
+
+
+
+ 
+/* Funcionamiento para no perderme al hacer los menús xD
+ Agregar los otros displays
+ Para el caso de que se ingrese la opcion 1: Ingresar al sistema 
+___________________________________________________________________________________________________
+ Caso ingresar al sistema: 
+    1. Atención cliente:
+        - Despliega otro menú en el cual se pueden ver todas las opciones de atencion al cliente
+    2. Información general de Préstamos:
+        - Despliega el menu con las opciones establecidas para este.
+___________________________________________________________________________________________________
+ Caso de crear una cuenta: 
+ -> Despliega menú con los requerimientos 
+    1. Nombre: (Recibe)
+    2. Tipo de cuente: (Recibe la opción)
+        - 1. Colones 
+        - 2. Dólares
+        - 3. Ambas
+    -> Imprime el id de cuenta 
+    -> Imprimir el cvv 
+    -> Imprimir el PIN
+___________________________________________________________________________________________________
+*/
