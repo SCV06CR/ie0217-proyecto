06ea4b668378d341d0379482d25d4f69152a9a73
jonathan0929 <79490210+jonathan0929@users.noreply.github.com>
2024-11-07 05:15:43 -0600

    Funcion para verificar el username
    Signed-off-by: jonathan0929 <79490210+jonathan0929@users.noreply.github.com>

---
+++ src/verificacionCaracteres.hpp
@@ -0,0 +1,15 @@
+#ifndef VERIFICAXION_NOMBRE_HPP
+#define VERIFICAXION_NOMBRE_HPP
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Funcion para validar el nombre
+string verificarNombre();
+
+// Funcion para validar la contrasena
+string verificarContrasena();
+
+#endif //VERIFICAXION_NOMBRE_HPP
