e98590a2defe5d5456399a803b8b1ea7a0a66c4c
Rodri Sanchez <rodrigo@Macboock-129.local>
2024-11-29 17:09:24 -0600

    Se elimina comentario de guia realizado al inicio
    Signed-off-by: Rodri Sanchez <rodrigo@Macboock-129.local>

--- src/main.cpp
+++ src/main.cpp
@@ -157,25 +157,4 @@ int main() {
 
 
  
-/* Funcionamiento para no perderme al hacer los menús xD
- Agregar los otros displays
- Para el caso de que se ingrese la opcion 1: Ingresar al sistema 
-___________________________________________________________________________________________________
- Caso ingresar al sistema: 
-    1. Atención cliente:
-        - Despliega otro menú en el cual se pueden ver todas las opciones de atencion al cliente
-    2. Información general de Préstamos:
-        - Despliega el menu con las opciones establecidas para este.
-___________________________________________________________________________________________________
- Caso de crear una cuenta: 
- -> Despliega menú con los requerimientos 
-    1. Nombre: (Recibe)
-    2. Tipo de cuente: (Recibe la opción)
-        - 1. Colones 
-        - 2. Dólares
-        - 3. Ambas
-    -> Imprime el id de cuenta 
-    -> Imprimir el cvv 
-    -> Imprimir el PIN
-___________________________________________________________________________________________________
-*/
+
